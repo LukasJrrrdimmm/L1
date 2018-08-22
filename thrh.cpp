74e61e9e04bd8e746278d1c167d6e09ece609234
Lukas Maximo Grilo Abreu Jardim <lukasmaximo@gmail.com>
2018-08-22 11:56:35 -0300

    Add files via upload

---
+++ thrh.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<thread>
+#include<vector>
+
+using namespace std;
+
+void systemVarUpgrade(int x, int myVar, int my_rank){
+	bool ok_for_1 = FALSE;
+	if(my_rank == 1)
+		while(!ok_for_1);
+	x += myvar
+	Unlock();
+	if(my_rank == 0)
+		ok_for_1 = TRUE;
+}
+
+int main(){
+	int x = 3, my_val = 0, my_rank = 0;
+	int my_val = Compute_val(my_rank)
+	vector<int> a;
+	vector<thread> b;
+	thread c, e;
+	c(systemVarUpgrade, x, my_val)
+	c.join();
+	return 0;
+}
+
+//  busywating
+/*
+
+Uma instrução do hardware que permite ler o estado de uma variável e alterar ela de forma atômica.
+
+
+Z(linguagem de programação)
+UPC(linguagem de programação)
+
+*/
